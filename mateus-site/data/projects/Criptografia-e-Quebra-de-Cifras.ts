7e3b269fa24effd408f574604840d7834a156d51
Mateus Amaral da Silva <silvaamaralmateus@gmail.com>
2026-03-12 15:42:11 -0300

    rpg text

--- mateus-site/data/projects/Criptografia-e-Quebra-de-Cifras.ts
+++ mateus-site/data/projects/Criptografia-e-Quebra-de-Cifras.ts
@@ -10,8 +10,6 @@ export const content: ContentBlock[] = [
   
   { type: 'section_header', value: '// Código'},
   { type: 'text', value: 'A ideia é simples, pegamos as palavras do alfabeto e mapeamos aleatoriamente para outras palavras, desta forma nosso algoritmo é mais robusto que cifras como a cira de cesar que apenas dão um shift arbitrário nas palavras.'},
-  { type: 'code', value: ''},
   { type: 'text', value: 'O proximo passo era lapidar o modelo, antes estavamos usando a ResNet34, agora tinhamos começado a usar a ResNet50, para conseguir rodar este modelo migramos para a plataforma própria de aprendizado centralizado e federado do laborátorio, o Flautim, um framework que utiliza do Flower para fornecer um ambiente de aprendizado federado.'},
-  { type: 'code', value: ''},
-  { type: 'text', value: 'Desta forma conseguimos ter um sistema completo de comunicação via cifras.'}
+  { type: 'text', value: 'Desta forma conseguimos ter um sistema completo de comunicação via cifras, caso tenha interesse em testar lembre-se que quanto maior o texto menos trabalho voccê terá na parte manual.'}
 ]
